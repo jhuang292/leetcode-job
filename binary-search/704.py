ce9910f50baec3c3d5fa6f4f80c33420ed0b77a0
jhuang292 <jhuang292@wisc.edu>
2019-03-26 18:35:33 -0500

    binary search

---
+++ binary-search/704.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0:
+            return -1
+        
+        start, end = 0, len(nums)-1
+        while start <= end:
+            mid = start + (end - start) // 2
+            if nums[mid] ==  target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return -1
